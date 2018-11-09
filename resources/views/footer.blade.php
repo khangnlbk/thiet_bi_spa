3984172af1163feb036326d623bd43e9e5a3adec
ChinIndival <tinhhang22@gmail.com>
2018-11-09 12:41:22 +0700

    fixLayout_part1

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,42 +3,46 @@
 		<div class="row">
 			<div class="col-sm-3">
 				<div class="widget">
-					<h4 class="widget-title">Hình ảnh</h4>
-					<div id="beta-instagram-feed"><div></div></div>
-				</div>
-			</div>
-			<div class="col-sm-2">
-				<div class="widget">
-					<h4 class="widget-title">Thông tin</h4>
+					<h4 class="widget-title"><center>Thông tin</center></h4>
+                    <hr>
 					<div>
 						<ul>
 							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Xây dựng</a></li>
 							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Phát triển</a></li>
 							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Quảng cáo</a></li>
-{{-- 							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Tips</a></li>
+        					<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Tips</a></li>
 							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Resources</a></li>
-							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Illustrations</a></li> --}}
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Illustrations</a></li>
 						</ul>
 					</div>
 				</div>
 			</div>
+            <div class="col-sm-1"></div>
 			<div class="col-sm-4">
-			 <div class="col-sm-10">
+			 <div class="col-sm-13">
 				<div class="widget">
-					<h4 class="widget-title">Liên hệ</h4>
+					<h4 class="widget-title"><center>Liên hệ</center></h4>
+                    <hr>
 					<div>
 						<div class="contact-info">
 							<i class="fa fa-map-marker"></i>
-							<p>Phạm Hoàng Hưng, Ngọc Thụy, Long Biên, Hà Nội. Điện thoại: +84 965 818 552</p>
-							<p>Website cho cửa hàng bánh trực tuyến - InternProject Lifetime Technology</p>
+							<p>Phạm Hoàng Hưng, Ngọc Thụy, Long Biên, Hà Nội. </p>
+                            <p>Điện thoại: +84 965 818 552</p>
 						</div>
+                        <br>
+                        <div class="contact-info">
+                            <i class="fa fa-map-marker"></i>
+                            <p>Website cho cửa hàng bánh trực tuyến - InternProject Lifetime Technology</p>
+                        </div>
 					</div>
 				</div>
 			  </div>
 			</div>
+            <div class="col-sm-1"></div>
 			<div class="col-sm-3">
 				<div class="widget">
-					<h4 class="widget-title">Theo dõi</h4>
+					<h4 class="widget-title"><center>Theo dõi</center></h4>
+                    <hr>
 					<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBamiBread%2F&tabs&width=250&height=150&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId" width="250" height="150" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 				</div>
 			</div>
